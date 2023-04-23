dab3a79405405588a5715f4525d52f8ff82a790d
Gonzalosilvalde <72249639+Gonzalosilvalde@users.noreply.github.com>
2023-04-23 14:34:05 +0200

    Update DecisionT.jl

--- src/algoritmos/DecisionT.jl
+++ src/algoritmos/DecisionT.jl
@@ -10,24 +10,18 @@ using Random:seed!
 using LinearAlgebra
 @sk_import tree: DecisionTreeClassifier
 
-
 function DecisionTree(modelHyperparameters::Dict, inputs::AbstractArray{<:Real,2}, targets::AbstractArray{<:Any,1}, crossValidationIndices::Array{Int64,1},numFold::Int)
     # Dividimos los datos en entrenamiento y test
     trainingInputs    = inputs[crossValidationIndices.!=numFold,:];
     testInputs        = inputs[crossValidationIndices.==numFold,:];
     trainingTargets   = targets[crossValidationIndices.!=numFold];
     testTargets       = targets[crossValidationIndices.==numFold];
-
     model = DecisionTreeClassifier(max_depth=modelHyperparameters["maxDepth"], random_state=1);
-
     # Entrenamos el modelo con el conjunto de entrenamiento
     model = fit!(model, trainingInputs, trainingTargets);
-
     # Pasamos el conjunto de test
     testOutputs = predict(model, testInputs);
-
     # Calculamos las metricas correspondientes con la funcion desarrollada en la practica anterior
     (acc, _, _, _, _, _, F1, _) = confusionMatrix(testOutputs, testTargets);
     return acc,F1;
 end
-
