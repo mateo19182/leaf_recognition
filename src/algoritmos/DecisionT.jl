14a4f68df66e7a1078bc206467afbaee61c4f502
Gonzalosilvalde <72249639+Gonzalosilvalde@users.noreply.github.com>
2023-04-30 14:17:57 +0200

    Update DecisionT.jl

--- src/algoritmos/DecisionT.jl
+++ src/algoritmos/DecisionT.jl
@@ -25,3 +25,20 @@ function DecisionTree(modelHyperparameters::Dict, inputs::AbstractArray{<:Real,2
     (acc, _, _, _, _, _, F1, _) = confusionMatrix(testOutputs, testTargets);
     return acc,F1;
 end
+
+function DecisionTree(modelHyperparameters::Dict, inputs::AbstractArray{<:Real,2}, targets::AbstractArray{<:Any,1})
+
+    dt = DecisionTreeClassifier(max_depth=modelHyperparameters["maxDepth"])
+
+    # Entrenamos el modelo con el todo el dataset
+    model = fit!(dt, inputs, targets);
+
+    # Pasamos el conjunto de test
+    testOutputs = predict(model, inputs);
+    
+    # Calculamos las metricas correspondientes con la funcion desarrollada en la practica anterior
+    (acc, _, _, _, _, _, F1, _) = confusionMatrix(testOutputs, targets);
+    printConfusionMatrix(testOutputs, targets; weighted=true);
+
+    return acc, F1;
+end
